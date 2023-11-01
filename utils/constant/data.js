8eee5b2187c4b6a265a7a38cb672c553ffa5bd9b
Karina Hapsari <karina.hapsari@mclinica.com>
2023-11-01 20:50:39 +0700

    Automated API Testing

---
+++ utils/constant/data.js
@@ -0,0 +1,6 @@
+export const DATA = {
+    API_URL: 'https://bookstore.toolsqa.com',
+    USERNAME: 'karina@gmail.com',
+    PASSWORD: 'Karina123!',
+    USERID: '42e33802-6cdb-4a52-ac08-c6b702e5c1a2',
+  };
